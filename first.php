e7559237a3c0141aaded24bf03e7b10fa61a95b6
Suraj Gupta <suraj.gupta1986@gmail.com>
2023-10-19 12:52:56 +0530

    gg

---
+++ first.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>
+</body>
+</html>
